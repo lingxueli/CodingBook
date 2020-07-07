144bc403cc45626a87fceadf362d712b59913d37
lingxueli <ll2871@columbia.edu>
2020-07-06 21:52:27 -0600

    first

--- Algorithm/BSTConstruction.py
+++ Algorithm/BSTConstruction.py
@@ -3,16 +3,21 @@
 # the insert, contains, and remove methods.
 # Feel free to add new properties and methods
 # to the class.
+
 class BST:
+
     def __init__(self, value):
         self.value = value
         self.left = None
         self.right = None
 
     def insert(self, value):
+
         # Write your code here.
         # Do not edit the return statement of this method.
-		node = self
+
+        node = self
+
         while True:
             if value < node.value:
                 if node.left == None:
@@ -29,7 +34,9 @@ class BST:
         return self
 
     def contains(self, value):
+
         # Write your code here.
+
         node = self
         while node != None:
             if value < node.value:
@@ -41,8 +48,10 @@ class BST:
         return False
 
     def remove(self, value):
+
         # Write your code here.
         # Do not edit the return statement of this method.
+
         node = self
         parent = self
         while node != None:
@@ -53,7 +62,7 @@ class BST:
                 parent = node
                 node = node.right
             else:
-                deleteNode = node.getMinNode()
+                deleteNode = node.right.getMinNode()
                 newNode = BST(deleteNode.value)
                 parent.right = newNode
                 newNode.left = node.left
@@ -67,3 +76,34 @@ class BST:
         while node.left != None:
             node = node.left
         return node
+
+
+root = BST(10)
+root.left = BST(5)
+root.right = BST(15)
+root.left.left = BST(2)
+root.left.right = BST(3)
+root.left.left.left = BST(1)
+root.right.left = BST(13)
+root.right.right = BST(22)
+
+# root.right.left.left = BST(12)
+
+root.right.left.right = BST(14)
+root.right.right.left = BST(21)
+
+root.insert(12)
+print(root.right.left.left.value)
+
+print(root.contains(23))
+
+# False
+
+print(root.contains(21))
+
+# True
+
+root.remove(15)
+print(root.right.value)
+
+# 21
