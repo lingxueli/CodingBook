1b5ffe976d53a12016531a88351c328c54775719
lingxueli <ll2871@columbia.edu>
2020-07-08 23:28:57 -0600

    first

--- Algorithm/BSTConstruction.py
+++ Algorithm/BSTConstruction.py
@@ -4,20 +4,20 @@
 # Feel free to add new properties and methods
 # to the class.
 
-class BST:
+# iterative works better than recursive
+# it has lower space complexity: iterative O(1) recursive O(logn)
+# time complexity are the same O(logn)
 
+class BST:
     def __init__(self, value):
         self.value = value
         self.left = None
         self.right = None
 
     def insert(self, value):
-
         # Write your code here.
         # Do not edit the return statement of this method.
-
-        node = self
-
+		node = self
         while True:
             if value < node.value:
                 if node.left == None:
@@ -34,9 +34,7 @@ class BST:
         return self
 
     def contains(self, value):
-
         # Write your code here.
-
         node = self
         while node != None:
             if value < node.value:
@@ -48,10 +46,8 @@ class BST:
         return False
 
     def remove(self, value):
-
         # Write your code here.
         # Do not edit the return statement of this method.
-
         node = self
         parent = self
         while node != None:
@@ -62,13 +58,33 @@ class BST:
                 parent = node
                 node = node.right
             else:
-                deleteNode = node.right.getMinNode()
-                newNode = BST(deleteNode.value)
-                parent.right = newNode
-                newNode.left = node.left
-                newNode.right = node.right
-                deleteNode = None
-                break
+				# hit the node that's going to be removed
+				# case 1: if the node to be removed has two child nodes
+                if node.left != None and node.right != None:
+					deleteNode = node.right.getMinNode()
+					newNode = BST(deleteNode.value)
+					parent.right = newNode
+					newNode.left = node.left
+					newNode.right = node.right
+					deleteNode = None
+
+				# case 2: if the node to be remove is leaf
+				elif node.left == None and node.right == None:
+					node = None
+
+				# case 3: if the node to be removed has one child
+				else:
+					if node.left != None:
+						if parent.left == node:
+							parent.left = node.left
+						else:
+							parent.right = node.left
+					else:
+						if parent.left == node:
+							parent.left = node.right
+						else:
+							parent.right = node.right
+				break
         return self
 
     def getMinNode(self):
@@ -76,7 +92,6 @@ class BST:
         while node.left != None:
             node = node.left
         return node
-
 
 root = BST(10)
 root.left = BST(5)
