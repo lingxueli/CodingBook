dcd9c80a1a32f0b3a035982fc0994a33ac4a6ddc
Lingxue Li <ll2871@columbia.edu>
2020-11-08 21:12:20 -0700

    test

--- Algorithm/h-index.py
+++ Algorithm/h-index.py
@@ -1,3 +1,5 @@
+from typing import List
+
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
         if len(citations) == 0:
@@ -17,3 +19,15 @@ class Solution:
                 
         #[100,99,98] h-index = 3        
         return len(citations)
+
+# time complexity: O(nlgn)
+# space complexity: constant
+
+s = Solution()
+print(s.hIndex([100,99,98])) # 3
+
+print(s.hIndex([3,0,6,1,5])) # 3
+
+print(s.hIndex([6,5,4,1,0])) # 3
+
+print(s.hIndex([6,4,1,0])) # 2
