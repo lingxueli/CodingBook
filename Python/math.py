3230d110cbe836ef6bd88672ddd5d021046708d4
Lingxue Li <ll2871@columbia.edu>
2020-12-27 21:46:13 -0700

    math

---
+++ Python/math.py
@@ -0,0 +1,31 @@
+# doc: python math functions
+
+print(2 ** 3) # 2 to the power of 3
+print(5 // 4) # divide and round up to
+print(6 % 4)
+
+print(round(3.1))
+print(abs(-3))
+
+# exercice
+print((5 + 4) * 10 / 2)
+# 45.0
+
+print(((5 + 4) * 10) / 2)
+# 45.0
+
+print((5 + 4) * (10 / 2))
+# 45.0
+
+print(5 + (4 * 10) / 2)
+# 25.0
+
+print(5 + 4 * 10 // 2)
+# 25
+
+# a new data type
+complex 
+
+# bin()
+print(bin(5)) # binary form of 5, 0b101
+print(int('0b101', 2))
