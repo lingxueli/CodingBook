937d6e17f9c2ec279f2778278e922bc62d0866af
Lingxue Li <ll2871@columbia.edu>
2020-12-30 23:39:28 -0700

    more about list

--- Python/list.py
+++ Python/list.py
@@ -30,3 +30,38 @@ new_cart = amazon_cart[:] # make a copy, assign it to the new one
 new_cart[0] = 'gum'
 print(new_cart)
 print(amazon_cart) # the two statements print out different lists
+
+
+# adding
+basket = [1,2,3,4,5]
+new_list= basket.append(1000) # append: modify the list in place
+print(basket)
+print(new_list) # None
+
+
+new_list = basket.insert(5,100) # insert: modify the list in place
+print(basket)
+print(new_list) # None
+
+
+new_list = basket.extend([100,101]) # extend: modify the list in place
+print(basket)
+print(new_list) # None
+
+# removing
+basket.pop() # pop from the end of the list
+print(basket)
+
+basket.pop(0) # pop from an index
+print(basket)
+
+new_list = basket.remove(4) # remove the value 4
+print(new_list) # remove: modify the list in place
+
+new_list = basket.pop(4) # pop: popped item is returned
+print(new_list) # new_list = popped item
+
+new_list = basket.clear()
+print(new_list) # None
+print(basket) # []
+
