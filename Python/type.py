90748bbf27c77f0bf42acf53b116e0325b9326f7
Lingxue Li <ll2871@columbia.edu>
2020-12-30 21:43:03 -0700

    type conversion

---
+++ Python/type.py
@@ -0,0 +1,17 @@
+# type conversion
+
+birth_year = input("what year were you born? ")
+age = 2020 - int(birth_year)
+
+print(f'your age is: {age}')
+
+# all the type conversion function
+bool
+int
+float
+complex
+str
+list
+tuple
+set
+dict
