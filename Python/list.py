d1a9733a884cba432f5b1b2865f80480a54df9a8
Lingxue Li <ll2871@columbia.edu>
2021-01-01 19:33:25 -0700

    more about list

--- Python/list.py
+++ Python/list.py
@@ -65,3 +65,66 @@ new_list = basket.clear()
 print(new_list) # None
 print(basket) # []
 
+# read
+basket = ['a','x','b','c','d','e','d']
+print(basket.index('d'))
+
+print(basket.index('d',0,6))
+
+print('a' in basket)
+
+print('f' in basket)
+
+print(basket.count('d'))
+
+# more list methods
+
+# list methond sort(): sort the list in place 
+basket.sort()
+print(basket) # sorted 
+
+basket = ['a','x','b','c','d','e','d']
+
+# built-in function sorted(): make a sorted copy of the original list
+print(sorted(basket)) # sorted
+print(basket) # not sorted
+
+# this has the same effect as sorted()
+new_basket = basket[:]
+new_basket.sort()
+print(new_basket) # sorted
+print(basket) # not sorted
+
+# list method copy()
+new_basket = basket.copy()
+new_basket.sort()
+print(new_basket) # sorted
+print(basket) # not sorted
+
+# list method reverse()
+basket.reverse() # not sorting, it reserves the indexes
+print(basket)
+
+# range -> list
+print(list(range(1,101)))
+print(list(range(101)))
+
+# join
+sentence = " "
+new_sentence = sentence.join(['hi','nice','to','meet','you'])
+print(sentence)
+print(new_sentence)
+
+# list unpacking
+basket = [1,2,3]
+a, b, c  = [1,2,3]
+print(a)
+print(b)
+print(c)
+
+a,b,c,*other = [1,2,3,4,5,6,7,8,9]
+print(other)
+
+a,b,c,*other,d = [1,2,3,4,5,6,7,8,9]
+print(other)
+print(d)
