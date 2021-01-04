cd88df75af5642ab33127d5f6691dfe161947607
Lingxue Li <ll2871@columbia.edu>
2021-01-03 21:42:39 -0700

    super argument

---
+++ Python/SuperargVsKwArg.py
@@ -0,0 +1,33 @@
+# *args **kwargs
+# arbitrary # of argument / keyword argument
+def super_func(*args, **kwargs):
+    print(args)
+    print(*args)
+    
+    print(kwargs)
+    print(*kwargs)
+
+    total = 0
+    for item in kwargs.values():
+        total += item
+        
+    return sum(args) + total
+
+print(super_func(1,2,3,4,5, num1 = 5, num2 = 10))
+
+# Rule: params, *args, default parameters, **kwargs
+def super_func(name, *args, i = 'hi', **kwargs):
+    print(args)
+    print(*args)
+    
+    print(kwargs)
+    print(*kwargs)
+
+    total = 0
+    for item in kwargs.values():
+        total += item
+        
+    return sum(args) + total
+
+print(super_func('Andy', 1,2,3,4,5, num1 = 5, num2 = 10))
+
