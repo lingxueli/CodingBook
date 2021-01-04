9b47d160d77bb9489fccf6ec42a560495b830686
Lingxue Li <ll2871@columbia.edu>
2021-01-03 21:58:06 -0700

    walrus operator

---
+++ Python/Basics/NewFeatures.py
@@ -0,0 +1,11 @@
+# what's new in python 3.8
+# read summary
+
+# walrus operator
+# :=
+
+a = 'helloooooooo'
+
+if ((n := len(a)) > 10):
+    # print(f"too long {len(a)} elements")
+    print(f"too long {n} elements")
