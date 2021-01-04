d6b2c99ed7d72723d6112bd233ca9439b2be84e7
Lingxue Li <ll2871@columbia.edu>
2021-01-03 21:59:36 -0700

    walrus operator

--- Python/Basics/NewFeatures.py
+++ Python/Basics/NewFeatures.py
@@ -9,3 +9,7 @@ a = 'helloooooooo'
 if ((n := len(a)) > 10):
     # print(f"too long {len(a)} elements")
     print(f"too long {n} elements")
+
+while ((n := len(a)) > 1):
+    print(n)
+    a = a[:-1] # remove the last character
